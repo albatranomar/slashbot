ecb160ed3d91ecf3fe0407d2724747ebb7053500
Omar Albatran <albatranomar943@gmail.com>
2022-07-07 05:44:08 +0000

    simple change on handlng commands

--- index.js
+++ index.js
@@ -19,8 +19,15 @@ client.once("ready", (client) => {
 });
 
 client.on('interactionCreate', async i => {
-	if (i.isCommand() && client.commands.has(i.commandName)) {
-		client.commands.get(i.commandName).execute(i);
+	if (i.isCommand()) {
+		let command = client.commands.get(i.commandName);
+		if (!command) return;
+		try {
+			await command.execute(i);
+		} catch (error) {
+			console.error(error);
+			await i.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
 	};
 });
 
